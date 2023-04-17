8d51a3e32f6a69b6ddcb3874a2ff6a7f7fe2fd38
hyunjee.kang <115131952+hyunjee-k@users.noreply.github.com>
2023-04-17 22:28:59 +0900

    Section 8: MVC in Phoenix

--- discuss/lib/discuss_web/controllers/page_controller.ex
+++ discuss/lib/discuss_web/controllers/page_controller.ex
@@ -1,5 +1,6 @@
 defmodule DiscussWeb.PageController do
-  use DiscussWeb, :controller
+  # discuss/lib/discuss_web.ex 에서 controller 를 사용하도록 설정
+  use Discuss.Web, :controller
 
   def home(conn, _params) do
     # The home page is often custom made,
---
+++ discuss/priv/repo/migrations/20230328150038_add_topics.exs
@@ -0,0 +1,11 @@
+defmodule Discuss.Repo.Migrations.AddTopics do
+  use Ecto.Migration
+
+  def change do
+    create table(:topics) do
+      add :title, :string
+    end
+  end
+end
+
+# 마이그레이션 파일이 순서대로 진행될 수 있도록 타임스탬프로 파일 명이 자동으로 정해진다.
